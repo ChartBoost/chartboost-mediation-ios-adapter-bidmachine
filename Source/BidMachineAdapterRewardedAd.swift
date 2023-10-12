c4bccf91fc292e6049618cb8914d4d012c1bd591
Alex Rice <rice.alexanderb@gmail.com>
2023-10-12 14:40:19 -0700

    Add rewarded ad

--- Source/BidMachineAdapterRewardedAd.swift
+++ Source/BidMachineAdapterRewardedAd.swift
@@ -8,22 +8,129 @@ import Foundation
 import BidMachine
 
 final class BidMachineAdapterRewardedAd: BidMachineAdapterAd, PartnerAd {
-    
+
     /// The BidMachineSDK ad instance.
-    var ad: BidMachineRewarded?
-    
+    private var ad: BidMachineRewarded?
+
     /// Loads an ad.
     /// - parameter viewController: The view controller on which the ad will be presented on. Needed on load for some banners.
     /// - parameter completion: Closure to be performed once the ad has been loaded.
     func load(with viewController: UIViewController?, completion: @escaping (Result<PartnerEventDetails, Error>) -> Void) {
         log(.loadStarted)
+
+        // Make request configuration
+        let config: BidMachineRequestConfigurationProtocol
+        do {
+            config = try BidMachineSdk.shared.requestConfiguration(.rewarded)
+        } catch {
+            let chartboostMediationError = self.error(.loadFailureUnknown, error: error)
+            log(.loadFailed(chartboostMediationError))
+            completion(.failure(chartboostMediationError))
+            return
+        }
+
+        loadCompletion = completion
+
+        // There's no harm in setting the placement ID when loading a bidding ad, but calling
+        // .withPayload(request.adm ?? "") causes an error when BidMachine parses the empty string
+        config.populate { $0.withPlacementId(request.partnerPlacement) }
+        if let adm = request.adm {
+            config.populate { $0.withPayload(adm) }
+        }
+
+        BidMachineSdk.shared.rewarded(config) { [weak self] ad, error in
+            guard let self = self else {
+                return
+            }
+            guard let ad = ad else {
+                let chartboostMediationError = self.error(.loadFailureUnknown, error: error)
+                log(.loadFailed(chartboostMediationError))
+                completion(.failure(chartboostMediationError))
+                return
+            }
+            self.ad = ad
+            ad.delegate = self
+            ad.loadAd()
+        }
     }
-    
+
     /// Shows a loaded ad.
     /// It will never get called for banner ads. You may leave the implementation blank for that ad format.
     /// - parameter viewController: The view controller on which the ad will be presented on.
     /// - parameter completion: Closure to be performed once the ad has been shown.
     func show(with viewController: UIViewController, completion: @escaping (Result<PartnerEventDetails, Error>) -> Void) {
         log(.showStarted)
+        ad?.controller = viewController
+        guard let ad = ad, ad.canShow else {
+            let error = error(.showFailureAdNotReady)
+            log(.showFailed(error))
+            completion(.failure(error))
+            return
+        }
+        showCompletion = completion
+        ad.presentAd()
     }
 }
+
+extension BidMachineAdapterRewardedAd: BidMachineAdDelegate {
+    func didLoadAd(_ ad: BidMachine.BidMachineAdProtocol) {
+        log(.loadSucceeded)
+        loadCompletion?(.success([:])) ?? log(.loadResultIgnored)
+        loadCompletion = nil
+    }
+
+    func didFailLoadAd(_ ad: BidMachine.BidMachineAdProtocol, _ error: Error) {
+        log(.loadFailed(error))
+        loadCompletion?(.failure(error)) ?? log(.loadResultIgnored)
+        loadCompletion = nil
+    }
+
+    func didPresentAd(_ ad: BidMachineAdProtocol) {
+        log(.showSucceeded)
+        showCompletion?(.success([:])) ?? log(.showResultIgnored)
+        showCompletion = nil
+    }
+
+    func didFailPresentAd(_ ad: BidMachineAdProtocol, _ error: Error) {
+        log(.showFailed(error))
+        showCompletion?(.failure(error)) ?? log(.showResultIgnored)
+        showCompletion = nil
+    }
+
+    func didDismissAd(_ ad: BidMachineAdProtocol) {
+        log(.delegateCallIgnored)
+    }
+
+    func willPresentScreen(_ ad: BidMachineAdProtocol) {
+        log(.delegateCallIgnored)
+    }
+
+    func didDismissScreen(_ ad: BidMachineAdProtocol) {
+        log(.delegateCallIgnored)
+    }
+
+    func didUserInteraction(_ ad: BidMachineAdProtocol) {
+        log(.didClick(error: nil))
+        delegate?.didClick(self, details: [:]) ?? log(.delegateUnavailable)
+    }
+
+    func didExpired(_ ad: BidMachineAdProtocol) {
+        log(.didExpire)
+        delegate?.didExpire(self, details: [:]) ?? log(.delegateUnavailable)
+    }
+
+    func didTrackImpression(_ ad: BidMachineAdProtocol) {
+        log(.didTrackImpression)
+        self.delegate?.didTrackImpression(self, details: [:]) ?? log(.delegateUnavailable)
+    }
+
+    func didTrackInteraction(_ ad: BidMachineAdProtocol) {
+        log(.delegateCallIgnored)
+    }
+
+    func didReceiveReward(_ ad: BidMachineAdProtocol) {
+        log(.didReward)
+        delegate?.didReward(self, details: [:]) ?? log(.delegateUnavailable)
+    }
+}
+
